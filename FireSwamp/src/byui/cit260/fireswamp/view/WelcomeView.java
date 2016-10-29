6646799219b38d00f6d2ff269ebc21e943f9e1bd
jared <jared@192.168.254.12>
2016-10-29 19:41:29 -0400

    Updated File 10/6/16

--- FireSwamp/src/byui/cit260/fireswamp/view/WelcomeView.java
+++ FireSwamp/src/byui/cit260/fireswamp/view/WelcomeView.java
@@ -15,10 +15,25 @@ public class WelcomeView {
     
     private void displayBanner(){
         
-        System.out.println("**************" 
-                +  "Welcome to the Fire Sawmp Game" 
-                +  "*****************");
-        
+        System.out.println(
+                "\n*         WELCOME TO THE FIRESWAMP         *"
+                +"\n*_*_*_*_*_*_*_*_*_*_*_*_*__*_*_*_*_*_*_*_*_*"
+                +"\n*                                          *"
+                +"\n*          Enter at your own risk!         *"
+                +"\n*     The Fire Swamp is full of Dangers    *"
+                +"\n*       You must navigate this Dangers     *"
+                +"\n*    To survive and get to the other side  *"
+                +"\n*          Watch out for the RoUs!         *"
+                +"\n*          Rodents of Unsual Size!         *"
+                +"\n*     Listen Carefully to the Fire Spurts  *"
+                +"\n*                    and                   *"
+                +"\n*             The Lightining Sand          *"
+                +"\n*        If you can survive you can        *"
+                +"\n*     Build your dream home in the swamp.  *"
+                +"\n*_*_*_*_*_*_*_*_*_*_*_*_*__*_*_*_*_*_*_*_*_*"
+                
+        );
+               
     }
     
     public void displayStartProgramView(){
