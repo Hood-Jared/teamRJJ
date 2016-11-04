e619985a609a4174a1adba9aa46ecc9f3aa67e5d
jared <jared@192.168.254.12>
2016-11-03 22:10:02 -0400

    Updated File 10/6/16

--- FireSwamp/test/byui/cit260/fireswamp/controller/DangerControllerTest.java
+++ FireSwamp/test/byui/cit260/fireswamp/controller/DangerControllerTest.java
@@ -5,8 +5,12 @@
  */
 package byui.cit260.fireswamp.controller;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -15,6 +19,22 @@ import static org.junit.Assert.*;
 public class DangerControllerTest {
     
     public DangerControllerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
 
     /**
@@ -33,6 +53,39 @@ public class DangerControllerTest {
         expResult = 3.14;
         result = instance.calcLightingSand(diameter);
         assertEquals(expResult, result, 0.1);
+    }
+
+    /**
+     * Test of CalcFireSpouts method, of class DangerController.
+     */
+    @Test
+    public void testCalcFireSpouts() {
+        System.out.println("CalcFireSpouts");
+        double triangleArea = 0.0;
+        DangerController instance = new DangerController();
+        double expResult = 0.0;
+        double result = instance.CalcFireSpouts(triangleArea);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    
+
+    /**
+     * Test of calcRous method, of class DangerController.
+     */
+    @Test
+    public void testCalcRous() {
+        System.out.println("calcRous");
+        double nextNumber = 0.0;
+        double lastNumber = 0.0;
+        DangerController instance = new DangerController();
+        double expResult = 0.0;
+        double result = instance.calcRous(nextNumber, lastNumber);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
     
 }
